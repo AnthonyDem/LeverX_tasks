2d85a773ca23450ad25eaa7a2ac27a42290ce788
AnthonyDem <thony.dem@gmail.com>
2020-02-17 12:17:09 +0300

    commit with lock

--- task3/task3.py
+++ task3/task3.py
@@ -1,22 +1,24 @@
-from threading import Thread
+from threading import Thread, Lock
+
 
 a = 0
 
 
 def function(arg):
     global a
+    lock = Lock()
     for _ in range(arg):
+        lock.acquire()
         a += 1
+        lock.release()
 
 
 def main():
-    threads = []
-    for i in range(5):
+    for _ in range(5):
         thread = Thread(target=function, args=(1000000,))
         thread.start()
-        threads.append(thread)
+        thread.join()
 
-    [t.join() for t in threads]
     print("----------------------", a)  # ???
 
 
