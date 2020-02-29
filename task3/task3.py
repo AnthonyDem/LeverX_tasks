655b48db6b61a1821626d7c9deb8a29b53c9577c
AnthonyDem <thony.dem@gmail.com>
2020-02-29 15:16:32 +0300

    correct task , make class

--- task3/task3.py
+++ task3/task3.py
@@ -1,27 +1,25 @@
 from threading import Thread, Lock
 import concurrent.futures
 
-lock = Lock()
+
+class Thread:
+    def __init__(self):
+        self.a = 0
+        self.lock = Lock()
+
+    def function(self, arg):
+        for _ in range(arg):
+            with self.lock:
+                self.a += 1
+
+    def main(self):
+        concurrency = 5
+        with concurrent.futures.ThreadPoolExecutor(concurrency) as executor:
+            for i in range(5):
+                executor.submit(self.function, 1000000, ).result()
+
+        print("----------------------", self.a)  # ???
 
 
-def function(arg):
-    a = 0
-    for _ in range(arg):
-        lock.acquire()
-        a += 1
-        lock.release()
-    return a
-
-
-def main():
-    concurrency = 5
-    a = 0
-    with concurrent.futures.ThreadPoolExecutor(concurrency) as executor:
-        for _ in range(5):
-            thread = executor.submit(function, 1000000)
-            a += thread.result()
-
-    print("----------------------", a)  # ???
-
-
-main()
+thread = Thread()
+thread.main()
